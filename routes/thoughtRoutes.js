17ed183a43b5432a7f627638317be41480c414ae
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-09-18 17:57:03 -0500

    Added the user and thought routes

---
+++ routes/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../controllers/thoughtController");
+
+// Routes for thoughts
+router.route("/").get(getThoughts).post(createThought);
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+router.route("/:thoughtId/reactions").post(addReaction).delete(removeReaction);
+
+module.exports = router;
