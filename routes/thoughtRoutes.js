4bc697f69b279d8ee14d2de2d636c535dd2aab4f
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-09-18 22:29:57 -0500

    Fixed reaction deletion route

--- routes/thoughtRoutes.js
+++ routes/thoughtRoutes.js
@@ -16,6 +16,7 @@ router
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
-router.route("/:thoughtId/reactions").post(addReaction).delete(removeReaction);
+router.route("/:thoughtId/reactions").post(addReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const db = require("./config/connection"); 
-const routes = require("./routes"); 
+const userRoutes = require("./routes/userRoutes");
+const thoughtRoutes = require("./routes/thoughtRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes); 
+app.use("/api/users", userRoutes);
+app.use("/api/thoughts", thoughtRoutes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
